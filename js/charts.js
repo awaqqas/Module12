fe071694d1f34573618f7322b3bc810cf9b085eb
awaqqas <attiyawaqqas@Attiyas-MacBook-Air.local>
2022-02-01 15:20:10 -0500

    Module12

--- js/charts.js
+++ js/charts.js
@@ -95,36 +95,51 @@ function buildCharts(sample) {
    };
     // 10. Use Plotly to plot the data with the layout. 
    Plotly.newPlot('bar', barData, barLayout);
-   var bubbleData = [
-    {
-     x: otu_ids,
-     y: sample_values,
-     text: otu_labels,
-     mode: "markers",
-     markers: {
-     color: ids,
-     size: values,
-     }
-   }
-
-   ];
-
-   // 2. Create the layout for the bubble chart.
-   var bubbleLayout = {
-   margin: { t: 0 },
-   xaxis: { title: "OTU ID" },
-   hovermode: "closest",
- 
-   };
-
-// 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
-    
   });
 }
 
+
   // Bar and Bubble charts
 // Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+    var result = filterArray[0];
+    var sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels; 
+    
+ Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          color: ids,
+          size: values,
+          }
+      }
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      margin: { t: 0 },
+      xaxis: { title: "OTU ID" },
+      hovermode: "closest",
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  });
+}
 
 
 
