9dafee945a67b217b7ef09a05b79f8f214eb473b
awaqqas <attiyawaqqas@Attiyas-MacBook-Air.local>
2022-02-01 16:29:38 -0500

    Module12

--- js/charts.js
+++ js/charts.js
@@ -1,28 +1,27 @@
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-  
+
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((sample) => {
-    var sampleNames = sample.names;
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
     sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
         .property("value", sample);
     });
-  
+
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
-
 }
-  
-  // Initialize the dashboard
-  init();
-  
+
+// Initialize the dashboard
+init();
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
@@ -30,7 +29,6 @@ function optionChanged(newSample) {
   buildCharts(newSample);
   
 }
-
 // Demographics Panel 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
