7375ef37918735c254ed01f8927ab833ef809072
awaqqas <attiyawaqqas@Attiyas-MacBook-Air.local>
2022-02-01 17:03:20 -0500

    Module12

--- js/charts.js
+++ js/charts.js
@@ -122,116 +122,18 @@ function buildCharts(sample) {
  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
   // 4. Create the trace for the gauge chart.
-  if (wfreq == null) {
-    wfreq = 0;
-}
-var traceGauge = {
-  domain: { x: [0, 1], y: [0, 1] },
-  value: wfreq,
-  type: "indicator",
-  mode: "gauge",
-  gauge: {
-      axis: {
-          range: [0, 9],
-          tickmode: 'linear',
-          tickfont: {
-              size: 15
-          }
-      },
-      bar: { color: 'rgba(8,29,88,0)' }, // making gauge bar transparent since a pointer is being used instead
-      steps: [
-          { range: [0, 1], color: 'rgb(255,255,217)' },
-          { range: [1, 2], color: 'rgb(237,248,217)' },
-          { range: [2, 3], color: 'rgb(199,233,180)' },
-          { range: [3, 4], color: 'rgb(127,205,187)' },
-          { range: [4, 5], color: 'rgb(65,182,196)' },
-          { range: [5, 6], color: 'rgb(29,145,192)' },
-          { range: [6, 7], color: 'rgb(34,94,168)' },
-          { range: [7, 8], color: 'rgb(37,52,148)' },
-          { range: [8, 9], color: 'rgb(8,29,88)' }
-      ]
-  }
-};
-
-// determine angle for each wfreq segment on the chart
-var angle = (wfreq / 9) * 180;
-
-// calculate end points for triangle pointer path
-var degrees = 180 - angle,
-  radius = .8;
-var radians = degrees * Math.PI / 180;
-var x = radius * Math.cos(radians);
-var y = radius * Math.sin(radians);
-
-// Path: to create needle shape (triangle). Initial coordinates of two of the triangle corners plus the third calculated end tip that points to the appropriate segment on the gauge 
-// M aX aY L bX bY L cX cY Z
-var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-  cX = String(x),
-  cY = String(y),
-  pathEnd = ' Z';
-var path = mainPath + cX + " " + cY + pathEnd;
-
-gaugeColors = ['rgb(8,29,88)', 'rgb(37,52,148)', 'rgb(34,94,168)', 'rgb(29,145,192)', 'rgb(65,182,196)', 'rgb(127,205,187)', 'rgb(199,233,180)', 'rgb(237,248,217)', 'rgb(255,255,217)', 'white']
-
-// create a trace to draw the circle where the needle is centered
-var traceNeedleCenter = {
-  type: 'scatter',
-  showlegend: false,
-  x: [0],
-  y: [0],
-  marker: { size: 35, color: '850000' },
-  name: wfreq,
-  hoverinfo: 'name'
-};
-
-// create a data array from the two traces
-var gaugeData = [traceGauge, traceNeedleCenter];
-    
   
+var gaugeData = [{
+  
+		domain: { x: [0, 1], y: [0, 1] },
+		value: 270,
+		title: { text: "Speed" },
+		type: "indicator",
+		mode: "gauge+number"
+	}]
   // 5. Create the layout for the gauge chart.
-  var gaugeLayout = { 
-      // draw the needle pointer shape using path defined above
-      shapes: [{
-        type: 'path',
-        path: path,
-        fillcolor: '850000',
-        line: {
-            color: '850000'
-        }
-    }],
-    font: {
-        family: 'Quicksand'
-    },
-    hoverlabel: {
-        font: {
-            family: 'Quicksand',
-            size: 16
-        }
-    },
-    title: {
-        text: `<b>Test Subject ${id}</b><br><b>Belly Button Washing Frequency</b><br><br>Scrubs per Week`,
-        font: {
-            size: 18,
-            color: 'rgb(34,94,168)'
-        },
-    },
-    height: 500,
-    width: 500,
-    xaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-        range: [-1, 1],
-        fixedrange: true // disable zoom
-    },
-    yaxis: {
-        zeroline: false,
-        showticklabels: false,
-        showgrid: false,
-        range: [-0.5, 1.5],
-        fixedrange: true // disable zoom
-    }
-};
+  var gaugeLayout = 
+      { width: 600, height: 500, margin: { t: 0, b: 0 } };
 
 
   // 6. Use Plotly to plot the gauge data and layout.
